79900c792d96caa609d7fb44320251e88d5db012
veronica365 <naruth10@gmail.com>
2023-03-23 20:40:52 +0300

    Fix how the sum function is exported

--- challenge.js
+++ challenge.js
@@ -1,7 +1,8 @@
-module.exports = function sum(number) {
+function sum(number) {
   if (number === 1) return 1;
   return number + sum(number - 1);
 };
+module.exports = sum;
 
 console.log(sum(4));
 console.log(sum(10));
