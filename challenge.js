0734447d4dd2c9592b1ea7ef87ec88902fe56fc3
veronica365 <naruth10@gmail.com>
2023-03-23 20:31:21 +0300

    Add functionality to handle the recurrsion

--- challenge.js
+++ challenge.js
@@ -1,6 +1,7 @@
-module.exports = function sum(number) {
-  // write your code here
+function sum(number) {
+  if (number === 1) return 1;
+  return number + sum(number - 1);
 }
 
-console.log(sum(4))
-console.log(sum(10))
+console.log(sum(4));
+console.log(sum(10));
