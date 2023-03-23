ed88298acee81b1c5d4944516237c89d6b4482b5
veronica365 <naruth10@gmail.com>
2023-03-23 20:34:26 +0300

    Add syntax to export the sum function

--- challenge.js
+++ challenge.js
@@ -1,7 +1,7 @@
-function sum(number) {
+module.exports = function sum(number) {
   if (number === 1) return 1;
   return number + sum(number - 1);
-}
+};
 
 console.log(sum(4));
 console.log(sum(10));
