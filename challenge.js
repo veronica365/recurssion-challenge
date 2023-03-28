5c68afc7e85d27ea0f949016f6cf27a31da61152
veronica365 <naruth10@gmail.com>
2023-03-28 10:22:51 +0300

    Add functionality to handle negative numbers

--- challenge.js
+++ challenge.js
@@ -1,8 +1,10 @@
 function sum(number) {
-  if (number === 1) return 1;
+  if (number < 1) return 0;
   return number + sum(number - 1);
 };
 module.exports = sum;
 
+console.log(sum(-4));
+console.log(sum(1));
 console.log(sum(4));
 console.log(sum(10));
