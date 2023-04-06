63df95c5dbddbe6020a3babef039e8a6c4ba4e92
veronica365 <naruth10@gmail.com>
2023-04-06 10:30:07 +0300

    Modify for negative numbers

--- challenge.js
+++ challenge.js
@@ -1,9 +1,11 @@
 function sum(number) {
-  if (number < 1) return 0;
+  if(number === 0) return number;
+  if (number < 0) return number + sum(number + 1);
   return number + sum(number - 1);
 };
 module.exports = sum;
 
+console.log(sum(-10));
 console.log(sum(-4));
 console.log(sum(1));
 console.log(sum(4));
